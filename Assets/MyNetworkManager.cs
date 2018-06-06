5619cac5f6e9ef8d1a2ee4654b7041e844e8c73d
danijel <danijel.husakovic@gmail.com>
2018-06-06 13:53:21 +0200

    Set myself up on two machines. Overriding 3 of the NetworkManager methods. The client is connected to the localhost IP. Pushing now so I can download from my second machine and see if the IP and time delay of connection(now it's 0ms) change :)

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	public void MyStartHost() {
+		StartHost ();
+		Debug.Log (Time.timeSinceLevelLoad + " Starting host.");
+	}
+
+	override public void OnStartHost(){
+		Debug.Log (Time.timeSinceLevelLoad + " Host started.");
+	}
+
+	override public void OnStartClient(NetworkClient myClient){
+		Debug.Log (Time.timeSinceLevelLoad + " Client start requested.");
+	}
+
+	override public void OnClientConnect(NetworkConnection conn){
+		Debug.Log (Time.timeSinceLevelLoad + " Client is connected to IP: " + conn.address);
+	}
+}
