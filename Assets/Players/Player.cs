d2098d51db974a062801ec4a9e342b2553b2d33f
danijel <danijel.husakovic@gmail.com>
2018-06-06 15:08:27 +0200

    The cube moved on the other machine! Now testing what happens if we have more players. Pushing to test on my second machine.

---
+++ Assets/Players/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	private Vector3 inputValue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		inputValue.x = CrossPlatformInputManager.GetAxis ("Horizontal");
+		inputValue.y = 0f;
+		inputValue.z = CrossPlatformInputManager.GetAxis ("Vertical");
+
+		transform.Translate (inputValue);
+	}
+}
